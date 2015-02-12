8e90a31ddc1ca9d6a952a196501bf3fb606e844c
Darren Patterson <darrenp1@stanford.edu>
2015-02-12 12:03:40 -0800

    root and sys update all fix to update all of writable agents instead of throw error on non-writeable in truly ALL

--- SyscheckReportClear.php
+++ SyscheckReportClear.php
@@ -12,36 +12,37 @@ $json['Result'] = 'Error';
 $json['Message'] = "SystemId provided ($id) was not found.";
 
 if (!is_numeric($id) and $id != 'all') {
-  $json['Result'] = "Error";
+  $json['Result'] = 'Error';
   $json['Message'] = "Provided SystemId is not an integer: $id";
 }
 else {
   if ($id == 'all') {
-    $systems = array_filter(explode("\n", shell_exec("sudo /var/ossec/bin/syscheck_control -ls")));
-    $ok = true;
-    foreach ($systems as $row) {
-      $r = array_filter(explode(",", $row));
-      if (!has_w_access($db, $_SERVER['REMOTE_USER'], $r[1])) {
-        $ok = false;
-        $json['Result'] = 'Error';
-        $json['Message'] = "Insufficient netdb privs for $r[1]";
-        break;
+    if (i_is_admin($_SERVER['REMOTE_USER'])) {
+      $json['Message'] = shell_exec("sudo /var/ossec/bin/syscheck_control -u $id");
+      $json['Result'] = 'OK';
+    }
+    else {
+      $systems = array_filter(explode("\n", shell_exec('sudo /var/ossec/bin/syscheck_control -ls')));
+      foreach ($systems as $row) {
+        $r = array_filter(explode(',', $row));
+        # Allow users to update "all" the servers that they can write to.
+        # Ignores all systems they don't have write access to.
+        if (has_w_access($db, $_SERVER['REMOTE_USER'], $r[1])) {
+          $json['Message'] .= shell_exec("sudo /var/ossec/bin/syscheck_control -u $r[0]");
+          $json['Result'] = 'OK';
+        }
       }
     }
     
-    if ($ok) {
-      $json["Message"] = shell_exec("sudo /var/ossec/bin/syscheck_control -u $id");
-      $json['Result'] = "OK";
-    }
   }
   else {
-    $systems = array_filter(explode("\n", shell_exec("sudo /var/ossec/bin/syscheck_control -ls")));
+    $systems = array_filter(explode("\n", shell_exec('sudo /var/ossec/bin/syscheck_control -ls')));
     foreach ($systems as $row) {
-      $r = array_filter(explode(",", $row));
+      $r = array_filter(explode(',', $row));
       if ($r[0] == $id) {
         if (has_w_access($db, $_SERVER['REMOTE_USER'], $r[1])) {
-          $json["Message"] = shell_exec("sudo /var/ossec/bin/syscheck_control -u $id");
-          $json['Result'] = "OK";
+          $json['Message'] = shell_exec("sudo /var/ossec/bin/syscheck_control -u $id");
+          $json['Result'] = 'OK';
         }
         else {
           $json['Result'] = 'Error';
